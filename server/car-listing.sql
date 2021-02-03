bccfc196fedb09bdc78dabeed89ce6508a1a2439
Abdelrahman Rizk <arizk1@outlook.com>
2021-02-03 14:25:26 +0100

    database SQL

---
+++ server/car-listing.sql
@@ -0,0 +1,20 @@
+DROP  TABLE IF EXISTS listings;
+DROP  TABLE IF EXISTS contacts;
+
+
+CREATE TABLE listings(
+    no SERIAL PRIMARY KEY,
+    id INT UNIQUE,
+    make VARCHAR(20),
+    price INT,
+    price_symbol VARCHAR(5) DEFAULT '€',
+    mileage INT,
+    seller_type VARCHAR(20)
+  );
+
+
+CREATE TABLE contacts(
+    id SERIAL PRIMARY KEY,
+    listing_id INT REFERENCES listings(id),
+    contact_date TIMESTAMP 
+);
