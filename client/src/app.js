8310fe994ba03f6f1490cfe336891bb25f32ddde
Abdelrahman Rizk <arizk1@outlook.com>
2021-02-03 14:02:37 +0100

    Upload input fields and Upload API endpoint

--- client/src/app.js
+++ client/src/app.js
@@ -1,12 +1,82 @@
-// import axios from axios;
-import { useState, useEffect } from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function App(){
+export default function App() {
+    const [csvFiles, setCsvFiles] = useState({});
+    const [error, setError] = useState(false);
+    const [message, setMessage] = useState();
+    const [views, setViews] = useState(1);
+
+    const handleFileChange = (e) => {
+        setCsvFiles({
+            ...csvFiles,
+            [e.target.name]: e.target.files[0],
+        });
+    };
+
+    const handleUpload = () => {
+        var formData = new FormData();
+        formData.append("file", csvFiles.contacts || csvFiles.listings);
+        // console.log(formData);
+
+        axios
+            .post("/upload-csvs", formData)
+            .then(({ data }) => {
+                console.log("response", data);
+                setViews(views + 1);
+
+                if (data.error) {
+                    setError(true);
+                    setMessage(data.message);
+                }
+            })
+            .catch((err) => {
+                console.log("error on POST /upload-csvs", err);
+            });
+    };
 
     return (
         <div>
-            <h1>HIIIII</h1>
+            <h1>Report Generator App</h1>
+            {error && <h3>{message}</h3>}
+
+            {views == 1 && (
+                <>
+                    <label>select the listings file </label>
+                    <input
+                        name="listings"
+                        type="file"
+                        accept=".csv"
+                        onChange={(e) => handleFileChange(e)}
+                        id="listings"
+                        className="inputfile"
+                    />
+                    <button onClick={(e) => handleUpload(e)}>Upload</button>
+                </>
+            )}
+            {views == 2 && (
+                <>
+                    <label>select the contacts file </label>
+                    <input
+                        name="contacts"
+                        type="file"
+                        accept=".csv"
+                        onChange={(e) => handleFileChange(e)}
+                        id="contacts"
+                        className="inputfile"
+                    />
+                    <button onClick={(e) => handleUpload(e)}>Upload</button>
+                </>
+            )}
+
+            {views == 3 && (
+                <>
+                    <Link to="/report1">Report 1</Link>
+                    <Link to="/report2">Report 2</Link>
+                    <Link to="/report3">Report 3</Link>
+                </>
+            )}
         </div>
-    )
+    );
 }
--- server/server.js
+++ server/server.js
@@ -8,19 +8,18 @@ const fs = require("fs");
 const d3 = require("d3");
 const _ = require("lodash");
 
-
 const compression = require("compression");
 const db = require("./db");
 
 const multer = require("multer");
-
+var cors = require("cors");
 
 //*##################################
 //*######    MIDDLEWARES     #######
 //*#################################
 
 app.use(compression());
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
 app.use(
     express.json({
@@ -34,13 +33,15 @@ const diskStorage = multer.diskStorage({
         callback(null, path.resolve(__dirname + "/uploads"));
     },
     filename: function (req, file, callback) {
-    callback(null, path.extname(file.originalname));
-            // .catch((err) => callback(err));
+        callback(null, Date.now() + "-" + file.originalname);
     },
 });
 
 const uploader = multer({
     storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
 });
 
 app.use(express.static("public"));
@@ -49,7 +50,31 @@ app.use(express.static("public"));
 //########### ROUTES ##############
 //#################################
 
-
+app.post("/upload-csvs", uploader.single("file"), (req, res) => {
+    const { filename } = req.file;
+    const filebath = `${__dirname}/uploads/${filename}`;
+    if (req.file) {
+        if (req.file.mimetype !== "text/csv") {
+            res.json({ message: "Only .csv files accepted!", error: true });
+        }
+        res.json({ success: true });
+        fs.readFile(filebath, "utf8", function (error, data) {
+            data = d3.csvParse(data);
+            if (data.columns.includes("listing_id")) {
+                console.log("contacts table");
+            } else if (data.columns.includes("seller_type")) {
+                console.log("listings table");
+            } else {
+                res.json({
+                    message: "This is not the correct file",
+                    error: true,
+                });
+            }
+        });
+    } else {
+        res.json({ error: true });
+    }
+});
 
 //##################################
 //####           /*           ######
@@ -62,4 +87,3 @@ app.get("*", function (req, res) {
 app.listen(process.env.PORT || 3001, function () {
     console.log("I'm listening.");
 });
-
